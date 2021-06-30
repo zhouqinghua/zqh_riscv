c297cc5d6d8498bc7d65f322c85b8b91a4dec28c
zhouqinghua <zhouqinghua888@163.com>
2021-06-30 12:21:59 +0800

    sv tc added: zqh_riscv_jtag_sba_test

--- verif/zqh_riscv/tests/test_master.sv
+++ verif/zqh_riscv/tests/test_master.sv
@@ -3,6 +3,7 @@ module test_master();
     zqh_vbase_log log;
     zqh_riscv_env env();
     zqh_riscv_sanity_test zqh_riscv_sanity_test();
+    zqh_riscv_jtag_sba_test zqh_riscv_jtag_sba_test();
     string test_name;
 
     initial begin
@@ -10,6 +11,9 @@ module test_master();
         $value$plusargs("zqh_test=%s", test_name);
         if (test_name == zqh_riscv_sanity_test.name) begin
             zqh_riscv_sanity_test.run_test();
+        end
+        else if (test_name == zqh_riscv_jtag_sba_test.name) begin
+            zqh_riscv_jtag_sba_test.run_test();
         end
         else begin
             `zqh_vbase_error(log, $display("unknow test: ", test_name));
